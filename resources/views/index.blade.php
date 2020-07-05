49ae4bcac7e01dd999bd08fbef923d00136da9c7
Yuta Sakuma <tyoromatu4423@gmail.com>
2020-07-05 21:04:28 +0900

    不要なjsファイルを削除、package.jsonのscriptを修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script src="{{mix('js/index.js')}}" defer></script>
         <title>react app</title>
     </head>
     <body>
         <div id="root"></div>
-        <script src="{{mix('js/index.js')}}"></script>
     </body>
 </html>
